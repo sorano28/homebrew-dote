86d07a4ea00d5e7b8cb7b59604d0f37709f2618e
sorano28 <kmc2531@kamiyama.ac.jp>
2025-10-28 11:47:12 +0900

    Remove version from dote.rb
    Remove version declaration from dote.rb

--- dote.rb
+++ dote.rb
@@ -3,7 +3,6 @@ class Dote < Formula
   homepage "https://github.com/sorano28/homebrew-dote"
   url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
   sha256 "0ae8837de3dbf1173450f0cfeed8b0b728bf09a32c78b4c234920f7c0b81b2be"
-  version “1.0.0”
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
