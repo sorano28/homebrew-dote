05fabf8aa8911b4da4ad0746e3c2c0450d33f26e
sorano28 <kmc2531@kamiyama.ac.jp>
2025-10-28 11:26:57 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -1,6 +1,6 @@
 class Dote < Formula
   desc "dot e command"
-  homepage "https://github.com/kmc2531/homebrew-dote"
+  homepage "https://github.com/sorano28/homebrew-dote"
   url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
   sha256 "0ae8837de3dbf1173450f0cfeed8b0b728bf09a32c78b4c234920f7c0b81b2be"
   version “1.0.0”
